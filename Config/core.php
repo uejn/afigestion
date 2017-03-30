796b296bf98dfb35933a976c067ada502e82f559
Ale Vilar <alevilar@gmail.com>
2017-03-30 15:24:50 -0300

    composer funcionando con test incluidos

--- Config/core.php
+++ Config/core.php
@@ -28,39 +28,9 @@ if (file_exists(APP . 'Config' . DS . 'croogo.php')) {
 }
 
 
-/**
-*
-*	Cantidad de registros a migrar
-*	Si se coloca false o 0 migra a todos
-*
-*	Se usa como "LIMIT" en la condicion del find
-**/
-Configure::write('Afigestion.AbstractMigratorLimit', false);
 
-Configure::write('Config.language', 'spa');
-
-Configure::write('Afigestion.descuento_mes', '');
-Configure::write('Afigestion.descuento_anio', '');
-
-Configure::write('Afigestion.carpeta_licencias_txt', '/var/www/html/TxtLicencias/diciembre');
-
-Configure::write('Afigestion.carpeta_descuentos_txt', '/var/www/html/txtDescuentos/diciembre');
-
-/*Path donde se encuentran los txt de licencias para la migracion mensual man*/
-Configure::write('Afigestion.carpeta_licencias_txt', '/var/www/html/TxtLicencias/diciembre');
-
-Configure::write('Afigestion.verificar_array_lindo', '/var/www/html/afi-gestion-marce/Vendor/afigestion/plugins/Afigestion/Test/TxtFilesForFixture/descuentos/');
-
-Configure::write('Afigestion.carpeta_descuentos_fallidos', '/var/www/html/afi-gestion-marce/Vendor/afigestion/plugins/Afigestion/Test/TxtFilesForFixture/descuentos/carpeta_con_archivo_mal_para_filemig/');
-
-Configure::write('DbTmpMig.migrators', array(
-                        //'NewsisAfiliadosDb',
-                        //'NewsisPadronDb',
-                        //'Menu28PadronDb',								
-                        //'Menu28AfiliadosDb',
-                        //'Menu28PadronTxt',								
-                        'NewsisConsejoPadronTxt',
-                        'NewsisCortePadronTxt',
-                        'NewsisDefensoriaPadronTxt',
-                        'NewsisProcuracionPadronTxt',					
-            ));
+if (file_exists(APP . 'Config' . DS . 'afigestion.php')) {
+    require APP . 'Config' . DS . 'afigestion.php';
+} else {
+   throw new CakeException("no se encuentra el archivo de configuracion de Config/afigestion.php, debera crearlo aunque este vacio")
+}
